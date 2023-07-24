9c1ab600d25d70fd56ac1c5357e1b97ae0f1f3f8
yuandasp <sitiyuandap@gmail.com>
2023-07-24 12:45:43 +0700

    create login and register (function and page)

--- src/components/Input.jsx
+++ src/components/Input.jsx
@@ -1,11 +1,11 @@
 import React from "react";
 
-function Input({ type = "text", name, placeholder = "" }) {
+function Input({ field, form: { touched, errors }, ...props }) {
   return (
     <input
-      type={type}
-      name={name}
-      placeholder={placeholder}
+      type="text"
+      {...field}
+      {...props}
       className="w-full border-solid border-slate-200 border-2 py-1 px-4 mb-4 rounded-sm"
     />
   );
--- src/pages/Login.jsx
+++ src/pages/Login.jsx
@@ -1,39 +1,162 @@
-import React from "react";
+import React, { useState } from "react";
 import Input from "components/Input";
 import Button from "components/Button";
-import { Link } from "react-router-dom";
+import { Link, useNavigate } from "react-router-dom";
+import * as Yup from "yup";
+import { Formik, Field, Form, ErrorMessage } from "formik";
+import Swal from "sweetalert2";
+import axios from "axios";
 
 function Login() {
-  return (
-    <div className="w-screen h-screen flex justify-center items-center">
-      <div className="w-1/3 border-solid border-slate-200 border-2 rounded-md flex box-shadow-register">
-        <div className="py-12 mx-auto px-6">
-          <div className="text-center">
-            <p className="font-bold text-3xl mb-6">Kanban</p>
-            <p className="font-bold mb-3">Log in to continue</p>
-            <div>
-              <Input type="text" name="email" placeholder="Enter Your Email" />
-              <Input
-                type="password"
-                name="password"
-                placeholder="Enter Your Password"
-              />
-            </div>
-            <Button text="Login" />
-          </div>
+  const [isLoading, setIsLoading] = useState(false);
+  const navigate = useNavigate();
 
-          <Link to={"/forget-password"}>
-            <p className="text-blue-700 text-center text-base my-4">
-              Forget password
-            </p>
-          </Link>
-          <Link to={"/register"}>
-            <p className="text-blue-700 text-center text-base my-2">
-              Don't have an account? Sign up
-            </p>
-          </Link>
-        </div>
-      </div>
+  const loginSchema = Yup.object().shape({
+    email: Yup.string()
+      .required("This field is required")
+      .email("Wrong email format"),
+    password: Yup.string()
+      .required("This field is required")
+      .min(8, "Password must be at least 8 characters"),
+  });
+
+  const loginUser = async (value, actions) => {
+    try {
+      setIsLoading(true);
+      let response = await axios.post(
+        `${process.env.REACT_APP_API_BE}/auth/login`,
+        value
+      );
+
+      Swal.fire({
+        icon: "success",
+        title: "Success!",
+        text: response.data.message,
+        footer: "",
+      });
+
+      setIsLoading(false);
+      navigate("/");
+    } catch (error) {
+      Swal.fire({
+        icon: "error",
+        title: "Oops...",
+        text: error.response.data?.message || "Something went wrong!!",
+      });
+      setIsLoading(false);
+    }
+  };
+
+  return (
+    <div>
+      <Formik
+        initialValues={{
+          email: "",
+          password: "",
+        }}
+        validationSchema={loginSchema}
+        onSubmit={(value, actions) => {
+          loginUser(value, actions);
+        }}
+      >
+        {(props) => {
+          return (
+            <>
+              <div className="flex min-h-screen h-full">
+                <div className="w-full lg:w-1/3 mx-auto flex justify-center flex-col p-14">
+                  <div>
+                    <p className="text-3xl font-bold mb-2">Hi again!</p>
+                    <p className="text-xl text-slate-500">
+                      Login to your account
+                    </p>
+                  </div>
+                  <div>
+                    <Form className="mt-8 space-y-6" action="#" method="POST">
+                      <input
+                        type="hidden"
+                        name="remember"
+                        defaultValue="true"
+                      />
+                      <div className="rounded-md">
+                        <div className="my-6">
+                          <label
+                            htmlFor="email-address"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            Email address
+                          </label>
+                          <div className="mt-2">
+                            <Field
+                              id="email"
+                              name="email"
+                              type="email"
+                              autoComplete="email"
+                              required
+                              className="shadow-sm pl-4 relative block w-full rounded-md border-0 py-6 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
+                              placeholder="Email"
+                              component={Input}
+                            />
+                          </div>
+                          <ErrorMessage
+                            component="div"
+                            name="email"
+                            style={{ color: "red", fontSize: "12px" }}
+                          />
+                        </div>
+
+                        <div className="my-6">
+                          <label
+                            htmlFor="password"
+                            className="block mb-2 text-sm font-medium text-gray-900 dark:text-white"
+                          >
+                            Password
+                          </label>
+                          <div className="mt-2">
+                            <Field
+                              id="password"
+                              name="password"
+                              type="password"
+                              required
+                              className="shadow-sm pl-4 relative block w-full rounded-md border-0 py-6 text-gray-900 ring-1 ring-inset ring-gray-300 placeholder:text-gray-400 focus:z-10 focus:ring-2 focus:ring-inset focus:ring-blue-900 sm:text-sm sm:leading-6"
+                              placeholder="Password"
+                              autoComplete="new-password"
+                              component={Input}
+                            />
+                          </div>
+                          <ErrorMessage
+                            component="div"
+                            name="password"
+                            style={{ color: "red", fontSize: "12px" }}
+                          />
+                        </div>
+                      </div>
+                      <div>
+                        <Button
+                          isLoading={isLoading}
+                          type="submit"
+                          className="w-full rounded-md py-6 text-white button-primary"
+                          text="Log in"
+                        ></Button>
+
+                        <div className="flex flex-wrap gap-2 items-end justify-end my-4">
+                          <p className="text-color-green text-end text-base lg:text-md">
+                            Don't have an account?
+                          </p>
+                          <Link to={"/register"}>
+                            <p className="text-color-green text-end font-bold text-md lg:text-lg hover:text-cyan-900">
+                              Register
+                            </p>
+                          </Link>
+                        </div>
+                      </div>
+                    </Form>
+                  </div>
+                </div>
+              </div>
+            </>
+          );
+        }}
+      </Formik>
     </div>
   );
 }
