c6bbb5c0ef82003772f5d8c68b29a649904b86b1
yuandasp <sitiyuandap@gmail.com>
2023-07-25 16:00:54 +0700

    get todo data based on status

--- src/components/TodoCard.jsx
+++ src/components/TodoCard.jsx
@@ -11,13 +11,17 @@ import {
 } from "@chakra-ui/react";
 import { useState } from "react";
 import AddTodoModal from "components/AddTodoModal";
-import { getAllTodos } from "features/todoSlice";
+import { getAllTodos, setStatusListToDo } from "features/todoSlice";
 import { useDispatch, useSelector } from "react-redux";
 
 function TodoCard() {
   const dispatch = useDispatch();
   const [isAddTodoOpenModal, setIsAddTodoOpenModal] = useState(false);
   const todos = useSelector((state) => state.todo.todos);
+
+  const statusTodo = todos.filter((todo) => todo.status === "todo");
+  const statusOnGoing = todos.filter((todo) => todo.status === "on going");
+  const statusDone = todos.filter((todo) => todo.status === "done");
 
   useEffect(() => {
     dispatch(getAllTodos());
@@ -34,15 +38,61 @@ function TodoCard() {
           <CardHeader className="bg-slate-200">
             <Heading size="md">Todo</Heading>
           </CardHeader>
-          {todos.map((todo) => (
-            <CardBody className="flex flex-col gap-8 bg-slate-200">
+          <CardBody className="flex flex-col gap-8 bg-slate-200">
+            {statusTodo.map((todo) => (
               <Card>
                 <CardBody>
                   <Text>{todo.description}</Text>
                 </CardBody>
               </Card>
-            </CardBody>
-          ))}
+            ))}
+          </CardBody>
+          <CardFooter className="bg-slate-200">
+            <Button
+              colorScheme="blue"
+              className="button-primary"
+              onClick={() => setIsAddTodoOpenModal(true)}
+            >
+              + Add a card
+            </Button>
+          </CardFooter>
+        </Card>
+        <Card>
+          <CardHeader className="bg-slate-200">
+            <Heading size="md">On going</Heading>
+          </CardHeader>
+          <CardBody className="flex flex-col gap-8 bg-slate-200">
+            {statusOnGoing.map((todo) => (
+              <Card>
+                <CardBody>
+                  <Text>{todo.description}</Text>
+                </CardBody>
+              </Card>
+            ))}
+          </CardBody>
+          <CardFooter className="bg-slate-200">
+            <Button
+              colorScheme="blue"
+              onClick={() => setIsAddTodoOpenModal(true)}
+            >
+              + Add a card
+            </Button>
+          </CardFooter>
+        </Card>
+
+        <Card>
+          <CardHeader className="bg-slate-200">
+            <Heading size="md">Done</Heading>
+          </CardHeader>
+          <CardBody className="flex flex-col gap-8 bg-slate-200">
+            {statusDone.map((done) => (
+              <Card>
+                <CardBody>
+                  <Text>{done.description}</Text>
+                </CardBody>
+              </Card>
+            ))}
+          </CardBody>
           <CardFooter className="bg-slate-200">
             <Button
               colorScheme="blue"
